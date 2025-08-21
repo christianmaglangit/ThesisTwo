c11147f89a1f6424c5663e8edad07e3b10431360
Christian Maglangit <157361777+christianmaglangit@users.noreply.github.com>
2025-08-21 16:27:21 +0800

    small add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from './homecomponents/navbar';
 import HomeContent1 from './homecomponents/homecontent1';
 import HomeContent2 from './homecomponents/homecontent2';
+import Dashdonor from './homecomponents/dashdonor';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <HomeContent1 />
       <HomeContent2 />
+      <Dashdonor />
     </>
   );
 }
